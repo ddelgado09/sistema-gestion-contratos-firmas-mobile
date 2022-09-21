ac4b553d64a4c20d5df5c010c20f5d06535a9469
Diego Delgado <ddelgado09@uan.edu.co>
2022-09-20 22:09:22 -0500

    set main file

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './src/navigation/Navigation';
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Hola mundo!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<NavigationContainer>
+			<Navigation />
+		</NavigationContainer>
 	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
