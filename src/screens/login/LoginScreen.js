d52ceec3b2824d4e1d0cc0bdb9a2a8507d971fba
Diego Delgado <ddelgado09@uan.edu.co>
2022-10-26 22:28:53 -0500

    build config

--- src/screens/login/LoginScreen.js
+++ src/screens/login/LoginScreen.js
@@ -15,6 +15,7 @@ export default function LoginScreen() {
         validationSchema: getValidationSchema(),
         validateOnChange: false,
         onSubmit: async ({ username, password }) => {
+            ToastAndroid.show(`${API_URL}login`);
             setLoading(true);
             try {
                 const response = await fetch(`${API_URL}login`, {
